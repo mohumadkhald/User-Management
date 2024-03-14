5daa47a45053dcf8ea908e410d8bb040bbae5cf9
mohumadkhald <mohumadkhald@gmail.com>
2024-03-14 22:20:04 +0200

    best

--- src/app/components/AddUser.tsx
+++ src/app/components/AddUser.tsx
@@ -1,12 +1,44 @@
-import React from 'react'
+"use client";
+
+import { Dialog, Transition } from '@headlessui/react'
+import React, { Fragment, useState } from 'react'
 
 const AddUser = () => {
+  let [isOpen, setIsOpen] = useState(true)
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
   return (
-    <div className='container mx-auto my-8'>
-      <div className='h-12 flex justify-end'>
-        <button className="rounded bg-slate-500 text-white px-6 py-2 font-semibold">Add User</button>
+    <>
+      <div className='container mx-auto my-8'>
+        <div className='h-12 flex justify-end'>
+          <button className="rounded bg-slate-500 text-white px-6 py-2 font-semibold">Add User</button>
+        </div>
       </div>
-    </div>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="fixed inset-0 z-10 overflow-y-auto" onClose={closeModal}>
+          <div className='min-h-screen px-4 text-center'></div>
+          <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <div className='inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-md'>
+                  <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-500"></Dialog.Title>
+                </div>
+              </Transition.Child>
+        </Dialog>
+      </Transition>
+    </>
   )
 }
 
--- src/app/components/UserList.tsx
+++ src/app/components/UserList.tsx
@@ -1,15 +1,15 @@
 "use client";
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import User from './User';
 import UserInterface from '../interface/user';
-import AddUser from './AddUser';
 
 const UserList = () => {
   const USER_API = "http://127.0.0.1:8080/api/v1/users";
   const [users, setUsers] = useState<UserInterface[]>([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null); // State variable to store error message
-  const [showPopup, setShowPopup] = useState(false); // State variable to control the visibility of the pop-up
+  const [error, setError] = useState<string | null>(null);
+  const [showPopup, setShowPopup] = useState(false);
+  const popupRef = useRef<HTMLDivElement>(null); // Ref to the pop-up div
 
   useEffect(() => {
     const fetchData = async () => {
@@ -29,56 +29,70 @@ const UserList = () => {
         setUsers(usersData);
       } catch (err) {
         if (err instanceof Error) {
-          setError(err.message); // Set error message if err is an instance of Error
+          setError(err.message);
         } else {
-          setError('An unknown error occurred'); // Fallback error message for unknown error types
+          setError('An unknown error occurred');
         }
-        setShowPopup(true); // Display the pop-up when an error occurs
+        setShowPopup(true);
       }
       setLoading(false);
     };
     fetchData();
   }, []);
 
+  useEffect(() => {
+    // Add event listener when the component mounts
+    window.addEventListener('click', handleClickOutside);
+    // Remove event listener when the component unmounts
+    return () => {
+      window.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
+  // Function to handle clicks outside the pop-up
+  const handleClickOutside = (event: MouseEvent) => {
+    if (popupRef.current && !popupRef.current.contains(event.target as Node)) {
+      setShowPopup(false); // Close the pop-up if the click occurs outside of it
+    }
+  };
+
   return (
-    <>
-      <div className='container mx-auto my-6'>
-        <div className='flex shadow border-b'>
-          <table className='min-w-full'>
-            <thead className='bg-gray-100'>
-              <tr>
-                <th className='text-gray-600 py-2 px-3'>ID</th>
-                <th className='text-gray-600 py-2 px-3'>Name</th>
-                <th className='text-gray-600 py-2 px-3'>Email</th>
-                <th className='text-gray-600 py-2 px-3'>Phone</th>
-                <th className='text-gray-600 py-2 px-3'>Address</th>
-                <th className='text-gray-600 py-2 px-3'>Actions</th>
-              </tr>
-            </thead>
-            {!loading && users && (
-              <tbody className='bg-white'>
-                {users.map((user) => (
-                  <User user={user} key={user.id}></User>
-                ))}
-              </tbody>
-            )}
-          </table>
-        </div>
-        {showPopup && (
-          <div className="fixed inset-2 flex items-center justify-center z-50">
-            <div className="relative bg-white p-8 mt-5 rounded shadow-md" style={{ width: 500, height: 250 }}>
+    <div className='container mx-auto my-6'>
+      <div className='flex shadow border-b'>
+        <table className='min-w-full'>
+          <thead className='bg-gray-100'>
+            <tr>
+              <th className='text-gray-600 py-2 px-3'>ID</th>
+              <th className='text-gray-600 py-2 px-3'>Name</th>
+              <th className='text-gray-600 py-2 px-3'>Email</th>
+              <th className='text-gray-600 py-2 px-3'>Phone</th>
+              <th className='text-gray-600 py-2 px-3'>Address</th>
+              <th className='text-gray-600 py-2 px-3'>Actions</th>
+            </tr>
+          </thead>
+          {!loading && users && (
+            <tbody className='bg-white'>
+              {users.map((user) => (
+                <User user={user} key={user.id}></User>
+              ))}
+            </tbody>
+          )}
+        </table>
+      </div>
+      {showPopup && (
+          <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+            <div ref={popupRef} className="relative bg-white p-8 mt-5 rounded shadow-md" style={{ width: 500, height: 250 }}>
               <div className="absolute inset-0 bg-gradient-to-br from-gray-100 to-white rounded shadow-md"></div>
               <div className="relative flex flex-col justify-between h-full">
                 <div className="bg-red-500 px-4 py-6 mt-7 text-white mb-4">{error}</div>
-                <div className="flex justify-end">
-                  <button className="bg-red-500 text-white px-4 py-2 rounded" onClick={() => setShowPopup(false)}>Close</button>
-                </div>
+                {/* <div className="flex justify-end">
+                  <button className="bg-red-500 text-white px-4 py-2 rounded hover:cursor-pointer" onClick={() => setShowPopup(false)}>Close</button>
+                </div> */}
               </div>
             </div>
-
           </div>
         )}
-      </div></>
+    </div>
   );
 };
 
